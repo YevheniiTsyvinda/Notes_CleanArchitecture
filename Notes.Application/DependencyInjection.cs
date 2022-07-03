8ac6f5b5323c9066800a8d8e570c31c5639345cd
Evhenii Tsyvinda <yevheniitsyvinda@gmail.com>
2022-07-03 15:03:11 +0300

    added validation

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,11 +1,8 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Notes.Application.Behaviors;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Notes.Application;
 
@@ -14,6 +11,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         return services;
     }
---
+++ Notes.Application/Notes/Commands/DeleteNote/DeleteNotCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Commands.DeleteNote;
+
+public class DeleteNotCommandValidator :AbstractValidator<DeleteNoteCommand>
+{
+    public DeleteNotCommandValidator()
+    {
+        RuleFor(deleteNoteCommand => deleteNoteCommand.Id).NotEqual(Guid.Empty);
+        RuleFor(deleteNoteCommand => deleteNoteCommand.UserId).NotEqual(Guid.Empty);
+    }
+}
